3ed0d07f002b915a31481d467bcecbe85c3d8a7f
Techno-Goats-9224 <ftcgoats9224@gmail.com>
2023-11-23 18:25:17 -0600

    made teleop work, auto almost working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AudienceRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AudienceRed.java
@@ -1,14 +1,18 @@
 
 package org.firstinspires.ftc.teamcode;
 
+        import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
         import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
         import com.qualcomm.robotcore.eventloop.opmode.OpMode;
         import com.qualcomm.robotcore.hardware.DcMotor;
         import com.qualcomm.robotcore.hardware.DcMotorEx;
         import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        import com.qualcomm.robotcore.hardware.IMU;
         import com.qualcomm.robotcore.hardware.PwmControl;
         import com.qualcomm.robotcore.hardware.Servo;
         import com.qualcomm.robotcore.hardware.ServoImplEx;
+
+        import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 /**
@@ -27,11 +31,82 @@ package org.firstinspires.ftc.teamcode;
 public class AudienceRed extends OpMode {
     private DcMotorEx rightBack;
     private DcMotorEx leftFront;
-    private DcMotorEx rightFront;
-    private DcMotorEx leftBack;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
     private ServoImplEx clawl;
     private ServoImplEx clawr;
     private DcMotorEx arm;
+    private IMU imu;
+    private enum directions{
+        FORWARD,
+        BACK,
+        LEFT,
+        RIGHT
+    }
+    public static double X_MULTIPLIER = 0.9787360469; // Multiplier in the X direction: 1.005395271
+    public static double Y_MULTIPLIER = 0.982667947; // Multiplier in/ the Y direction
+    public static double TICKS_PER_REV = 4000;
+    public static double WHEEL_RADIUS = 1; // in
+    public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
+    public static double encoderTicksToInches(double ticks) {
+        return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+    }
+
+    public void drive(double inches, directions dir) {
+        if (dir == directions.FORWARD) {
+            while (encoderTicksToInches(leftFront.getCurrentPosition()) > -inches && opModeIsActive()) {
+                leftFront.setPower(-.31);
+                leftBack.setPower(-.31);
+                rightFront.setPower(.31);
+                rightBack.setPower(-.31);
+
+                telemetry.addData("front left (para)", leftFront.getCurrentPosition());
+                telemetry.update();
+            }
+        }
+        if (dir == directions.BACK) {
+            while (encoderTicksToInches(leftFront.getCurrentPosition()) < inches) {
+                leftFront.setPower(.31);
+                leftBack.setPower(.31);
+                rightFront.setPower(-.31);
+                rightBack.setPower(.31);
+            }
+        }
+        if (dir == directions.LEFT) {
+            while (encoderTicksToInches(rightBack.getCurrentPosition()) > -inches) {
+                leftFront.setPower(-.31);
+                leftBack.setPower(.31);
+                rightFront.setPower(-.31);
+                rightBack.setPower(.31);
+            }
+        }
+        if (dir == directions.RIGHT) {
+            while (encoderTicksToInches(rightBack.getCurrentPosition()) < inches) {
+                leftFront.setPower(.31);
+                leftBack.setPower(-.31);
+                rightFront.setPower(.31);
+                rightBack.setPower(.31);
+            }
+        }
+    }
+    public void turn(double degrees, directions dir) {
+        if (dir == directions.LEFT) {
+            while (imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES) > -degrees) {
+                leftFront.setPower(-.31);
+                leftBack.setPower(-.31);
+                rightFront.setPower(.31);
+                rightBack.setPower(-.31);
+            }
+        }
+        if (dir == directions.RIGHT) {
+            while (imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES) < degrees) {
+                leftFront.setPower(.31);
+                leftBack.setPower(.31);
+                rightFront.setPower(-.31);
+                rightBack.setPower(.31);
+            }
+        }
+    }
     @Override
     public void init() {
         leftBack = hardwareMap.get(DcMotorEx.class,"leftBack");
@@ -41,25 +116,36 @@ public class AudienceRed extends OpMode {
         arm = hardwareMap.get(DcMotorEx.class,"arm");
         clawl = hardwareMap.get(ServoImplEx.class,"clawl");
         clawr = hardwareMap.get(ServoImplEx.class,"clawr");
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
 
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            clawl.setPwmRange(new PwmControl.PwmRange(500, 2500));
-            clawr.setPwmRange(new PwmControl.PwmRange(500, 2500));
-            clawl.setDirection(Servo.Direction.REVERSE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        clawl.setPwmRange(new PwmControl.PwmRange(500, 2500));
+        clawr.setPwmRange(new PwmControl.PwmRange(500, 2500));
+        clawl.setDirection(Servo.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
     }
 
-        // run until the end of the match (driver presses STOP)
     @Override
     public void init_loop() {
     }
@@ -67,23 +153,44 @@ public class AudienceRed extends OpMode {
     public void start() {
         //Pixy look for team prop
         //Robot needs to drive and move forward like 24in ish
+        drive(24, directions.FORWARD);
         //Drop pixel at left: turn left 90 degrees then open claw then turn right to get back on track.
-        //Drop pixel at center: drive past 24in then turn around 180 degrees and then drop pixel and then turn another 180 degrees.
-        //Drop pixel right: Turn right 90 degrees then open claw pick up pixel then turn left 90 degrees then drive forward 24in
+       // turn(90, directions.LEFT);
+       // clawl.setPosition(0.4);
+       // turn(90, directions.RIGHT);
+        // Drop pixel at center: drive past then turn around 180 degrees and then drop pixel and then turn another 180 degrees.
+        //clawl.setPosition(0.4);
+       // drive(24, directions.FORWARD);
+        //turn(180, directions.RIGHT);
+        //clawl.setPosition(0.4);
+        //turn(180, directions.RIGHT);
         //Then turn right 90 degrees
+        //turn(90, directions.RIGHT);
         //Drive the remaining 48in
+        //drive(48, directions.FORWARD);
         //Then turn 90 degrees to the right after the 72in
+       // turn(90, directions.RIGHT);
         //After that drive forward 96in underneath the stage door
+       // drive(96, directions.FORWARD);
         //Then turn another 90 degrees to the right
+        //turn(90, directions.RIGHT);
         //Then Drive forward 24in
+       // drive(24, directions.FORWARD);
         //Then turn another 90 degrees the left
+        //turn(90, directions.LEFT);
         //Then april tag will direct robot to backdrop
     }
     @Override
     public void loop() {
+        telemetry.addData("Parallel encoder", leftFront.getCurrentPosition());
+        telemetry.addData("perpendicular encoder", rightBack.getCurrentPosition());
     }
     @Override
     public void stop() {
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 //list of controller function here: https://docs.google.com/document/d/1mTPaoFG1fvQqmZDU4-IlvfgqJ6lwRfTvbBcxseAkrCM/edit?usp=sharing
 @TeleOp
-    public class Teleop extends OpMode {
+public class Teleop extends OpMode {
     private DcMotorEx rightBack;
     private DcMotorEx leftFront;
     private DcMotorEx rightFront;
@@ -23,95 +23,116 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
     private ServoImplEx clawl;
     private ServoImplEx clawr;
     private Servo drone;
-        @Override
-        public void init() {
-            intakel = hardwareMap.get(DcMotorEx.class,"intakel");
-            intaker = hardwareMap.get(DcMotorEx.class,"intaker");
-            arm = hardwareMap.get(DcMotorEx.class,"arm");
-            clawl = hardwareMap.get(ServoImplEx.class,"clawl");
-            clawr = hardwareMap.get(ServoImplEx.class,"clawr");
-            leftBack = hardwareMap.get(DcMotorEx.class,"leftBack");
-            rightBack = hardwareMap.get(DcMotorEx.class,"rightBack");
-            leftFront = hardwareMap.get(DcMotorEx.class,"leftFront");
-            rightFront = hardwareMap.get(DcMotorEx.class,"rightFront");
-            drone = hardwareMap.get(Servo.class,"drone");
+    private Servo rotate;
+    @Override
+    public void init() {
+        intakel = hardwareMap.get(DcMotorEx.class,"intakel");
+        intaker = hardwareMap.get(DcMotorEx.class,"intaker");
+        arm = hardwareMap.get(DcMotorEx.class,"arm");
+        clawl = hardwareMap.get(ServoImplEx.class,"clawl");
+        clawr = hardwareMap.get(ServoImplEx.class,"clawr");
+        leftBack = hardwareMap.get(DcMotorEx.class,"leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class,"rightBack");
+        leftFront = hardwareMap.get(DcMotorEx.class,"leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class,"rightFront");
+        drone = hardwareMap.get(Servo.class,"drone");
+        rotate = hardwareMap.get(Servo.class, "rotate");
 
-            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-            leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-            rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-            leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            clawl.setPwmRange(new PwmControl.PwmRange(500, 2500));
-            clawr.setPwmRange(new PwmControl.PwmRange(500, 2500));
-            clawl.setDirection(Servo.Direction.REVERSE);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            telemetry.addData("Status", "Initialized");
-            telemetry.update();
+        clawl.setPwmRange(new PwmControl.PwmRange(500, 2500));
+        clawr.setPwmRange(new PwmControl.PwmRange(500, 2500));
+        clawl.setDirection(Servo.Direction.REVERSE);
+        clawr.setDirection(Servo.Direction.REVERSE);
 
-        }
-        @Override
-        public void init_loop() {
-        }
-        @Override
-        public void start() {
-        }
-        @Override
-        public void loop() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-            if (gamepad2.dpad_up) {
-                arm.setPower(1);
-            }else if(gamepad2.dpad_down){
-                arm.setPower(-1);
-            }else {
-                arm.setPower(0);
-            }
-            if (gamepad2.cross){
-                intakel.setPower(-1);
-                intaker.setPower(1);
-            }else {
-                intakel.setPower(0);
-                intaker.setPower(0);
-            }
-            if(gamepad2.square) {
-                clawl.setPosition(.6);
-            }else {
-                clawl.setPosition(0.4);
-            }
-            if(gamepad2.circle) {
-                clawr.setPosition(.25);
-            }else {
-                clawr.setPosition(.05);
-            }
-            if (gamepad2.triangle){
-                drone.setPosition(.5);
-            }else{
-                drone.setPosition(1);
-                //1 is when not pushed and .5 is when pushed
-                // 1 is holding rubberband .5 is out
-            }
-
-            double ly = gamepad1.left_stick_y * 0.8;
-            double lx = -gamepad1.left_stick_x * 0.8;
-            double rx = -gamepad1.right_stick_x * 0.8;
-
-            leftFront.setPower(ly + lx + rx);
-            leftBack.setPower(ly - lx + rx);
-            rightFront.setPower(-ly + lx + rx);
-            rightBack.setPower(ly + lx - rx);
-
-            telemetry.addData("left claw servo position", clawl.getPosition());
-            telemetry.addData("right claw servo position", clawr.getPosition());
-            telemetry.addData("drone servo position", drone.getPosition());
-            telemetry.addData("Front Left Encoder", leftFront.getCurrentPosition());
-            telemetry.addData("Front Right Encoder", rightBack.getCurrentPosition());
-        }
-        @Override
-        public void stop() {
-
-        }
     }
+    @Override
+    public void init_loop() {
+    }
+    @Override
+    public void start() {
+    }
+    @Override
+    public void loop() {
 
+        if (gamepad2.dpad_up) {
+            arm.setPower(1);
+        }else if(gamepad2.dpad_down){
+            arm.setPower(-1);
+        }else {
+            arm.setPower(0);
+        }
+        if (gamepad2.cross){
+            intakel.setPower(-1);
+            intaker.setPower(1);
+        }else {
+            intakel.setPower(0);
+            intaker.setPower(0);
+        }
+        //left maybe not working, need to test
+        if(gamepad2.square) {
+            clawl.setPosition(0.6);
+        }else {
+            //open
+            clawl.setPosition(0.45);
+        }
+        if(gamepad2.left_bumper){
+            rotate.setPosition(0.1);
+        }else{
+            rotate.setPosition(0);
+        }
+        if(gamepad2.circle) {
+            //close
+            clawr.setPosition(.05);
+        }else {
+            //open
+            clawr.setPosition(.25);
+        }
+        if (gamepad2.triangle){
+            drone.setPosition(.5);
+        }else{
+            drone.setPosition(1);
+            //1 is when not pushed and .5 is when pushed
+            // 1 is holding rubberband .5 is out
+        }
 
+        double ly = gamepad1.left_stick_y * 0.8;
+        double lx = -gamepad1.left_stick_x * 0.8;
+        double rx = -gamepad1.right_stick_x * 0.8;
+
+        leftFront.setPower(ly + lx + rx);
+        leftBack.setPower(ly - lx + rx);
+        rightFront.setPower(-ly + lx + rx);
+        rightBack.setPower(ly + lx - rx);
+
+        telemetry.addData("front left power", leftFront.getPower());
+        telemetry.addData("front right power", rightFront.getPower());
+        telemetry.addData("back left power", leftBack.getPower());
+        telemetry.addData("back right power", rightBack.getPower());
+
+        telemetry.addData("left claw servo position", clawl.getPosition());
+        telemetry.addData("right claw servo position", clawr.getPosition());
+        telemetry.addData("drone servo position", drone.getPosition());
+        telemetry.addData("Front Left Encoder (perp)", leftFront.getCurrentPosition());
+        telemetry.addData("Negative Back Right Encoder (para)", -rightBack.getCurrentPosition());
+    }
+    @Override
+    public void stop() {
+
+    }
+}
